246fccdeb44295f7220f9cd86a2844a98126e365
minju <jmjchai0509@ewhain.net>
2023-12-28 14:28:26 +0900

    Refactor: 코드 변수 수정
    MoveToNextScene 파일 변수명 수정

--- EPOH/Assets/Scripts/MoveToNextScene.cs
+++ EPOH/Assets/Scripts/MoveToNextScene.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 
 public class MoveToNextScene : MonoBehaviour
 {
-    public string nextSceneName = "NextScene"; // 다음 씬의 이름을 설정할 변수
+    public string next_scene_name = "NextScene"; // 다음 씬의 이름을 설정할 변수
 
     public void sceneChange()
     {
-        SceneManager.LoadScene(nextSceneName); // nextSceneName 변수를 사용하여 씬을 로드
+        SceneManager.LoadScene(next_scene_name); // nextSceneName 변수를 사용하여 씬을 로드
     }
 
   
