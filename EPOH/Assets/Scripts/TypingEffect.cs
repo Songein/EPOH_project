e291e5f681441325d3b36bc7cd22e0e60d6149b5
Song Ein <songein@ewhain.net>
2024-01-04 13:50:43 +0900

    Feat: 스토리 진행 구현
    대화창을 통한 스토리 진행 파트 구현
    TalkAction, TalkManger, TypingEffect 스크립트 작성
    
    TalkManager
    - 스토리 스크립트 Dictionary
    - 스크립트를 한 줄씩 반환하는 GetTalk함수
    
    TalkAction
    - 대화를 시작하거나 진행하는 Action 함수
    - 스크립트가 있는지 확인하고 그 여부에 따라 대화창을 SetActive 및 대사를 출력하는 Talk 함수
    
    TypingEffect
    - Invoke 함수를 이용해 글자가 하나씩 출력되는 효과

---
+++ EPOH/Assets/Scripts/TypingEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TypingEffect : MonoBehaviour
+{
+    string target_message; // 출력할 메세지
+    public int char_per_sec; // 1초에 몇 개의 글자를 출력하는지
+    public TextMeshProUGUI message_text;
+    int index;
+
+    public void SetMessage(string message)
+    {
+        target_message = message; // 출력할 메세지를 저장
+        EffectStart();
+    }
+
+    void EffectStart()
+    {
+        message_text.text = ""; // 대화창의 text를 비운다
+        index = 0; // 인덱스 초기화
+
+        Invoke("Effecting", 1.0f /char_per_sec); // Invoke를 사용해 시간을 지연시켜서 출력
+    }
+    void Effecting()
+    {
+        if(message_text.text == target_message) // 문장을 전부 출력했을 경우
+        {
+            return; // 함수 종료
+        }
+        message_text.text += target_message[index];
+        index++;
+        
+        Invoke("Effecting", 1.0f / char_per_sec); // 재귀적으로 호출
+    }
+}
