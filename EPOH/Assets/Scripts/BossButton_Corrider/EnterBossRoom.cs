201b7bd1dcc7581bfa6b7e5c706105aa9a698fa0
MinJu <jmjchai0509@ewhain.net>
2024-01-09 01:25:59 +0900

    Refactor: 보스룸 씬 이름설정 방법수정
    EnterBossRoom.cs 에서
    
    1) moveToBossRoom()에서 보스룸 이름 설정을 할 때 switch를 사용하지 않고 작명규칙에 따라 GameManager의 boss_info 첫번째 요소 확인해서 BossRoom + "보스명"으로 설정하는 것으로 변경
    2) 스크립트 내 함수명(카멜 방식)/변수명(언더바) 형식으로 변경
    
    보스룸 Scene들 이름 변경
    BossRoom_Dog -> BossRoomDog
    BossRoom_ForgetMeNot ->BossRoomForgetMeNot
    BossRoom_PartTime -> BossRoomPartTime

--- EPOH/Assets/Scripts/BossButton_Corrider/EnterBossRoom.cs
+++ EPOH/Assets/Scripts/BossButton_Corrider/EnterBossRoom.cs
@@ -4,52 +4,42 @@ using UnityEngine.SceneManagement;
 
 public class EnterBossRoom : MonoBehaviour
 {
-    public Button moveToRightButton; // MoveToRight device 버튼
-    private GameManager gameManager; // GameManager 스크립트에 대한 참조 변수
+    public Button move_to_right_button; // MoveToRight device 버튼
+    private GameManager game_manager; // GameManager 스크립트에 대한 참조 변수
 
     void Start()
     {
-        if (moveToRightButton != null)
+        if (move_to_right_button != null)
         {
-            moveToRightButton.onClick.AddListener(() => MoveToBossRoom());
+            move_to_right_button.onClick.AddListener(() => moveToBossRoom());
         }
 
-        gameManager = FindObjectOfType<GameManager>();
+        game_manager = FindObjectOfType<GameManager>();
 
-        if (gameManager == null)
+        if (game_manager == null)
         {
             Debug.LogError("GameManager를 찾을 수 없습니다.");
             return;
         }
-
-        
     }
 
-    void MoveToBossRoom()
+    void moveToBossRoom()
     {
-        int bossIndex = gameManager.boss_num;
-        string bossRoomSceneName = "";
+        int boss_index = game_manager.boss_num;
+        string boss_room_scene_name = "";
 
-        // 보스 인덱스에 따라 보스 룸 씬의 이름 설정
-        switch (bossIndex)
+        if (boss_index >= 0 && boss_index < GameManager.boss_cnt)
         {
-            case 0:
-                bossRoomSceneName = "BossRoom_Dog";
-                break;
-            case 1:
-                bossRoomSceneName = "BossRoom_ForgetMeNot";
-                break;
-            case 2:
-                bossRoomSceneName = "BossRoom_PartTime";
-                break;
-            default:
-                Debug.LogWarning("해당하는 보스 룸 씬을 찾을 수 없습니다.");
-                break;
+            boss_room_scene_name = "BossRoom" + game_manager.boss_info[boss_index, 0]; // 보스 룸 씬의 이름 설정
+        }
+        else
+        {
+            Debug.LogWarning("해당하는 보스 룸 씬을 찾을 수 없습니다.");
+            return;
         }
 
         // 보스 룸 씬으로 전환
-        SceneManager.LoadScene(bossRoomSceneName);
+        SceneManager.LoadScene(boss_room_scene_name);
     }
-
-    
 }
+ 
