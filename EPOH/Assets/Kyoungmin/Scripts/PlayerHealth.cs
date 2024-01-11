6f8a6821d3345898c6ecd08cf20fff0b217323ae
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2024-01-11 14:30:46 +0900

    Feat : 플레이어 무적시간 및 사망 기능
    - 플레이어가 공격 받을 경우와 죽을 때 애니메이션 아직 구현하지 않음.
    - 실제 그래픽으로 변경 시, 콤보 공격마다 AttackArea의 collider를 조절하여 공격에 맞는 콜라이더가 나타나도록 조정해야 함.
    -BossManager.cs에서 플레이어와 보스의 hp를 관리하지 않고 각각 PlayerHealth.cs와 BossHealth.cs를 통해 관리함.

---
+++ EPOH/Assets/Kyoungmin/Scripts/PlayerHealth.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float player_hp = 200; //플레이어의 목숨
+    private SpriteRenderer sp; //플레이어 SpriteRenderer 참조
+    
+    void Start()
+    {
+        //SpriteRenderer 할당하기
+        sp = GetComponent<SpriteRenderer>();
+    }
+    
+    //플레이어 데미지 관련
+    public void Damage(float power)
+    {
+        Debug.Log("[PlayerHealth] : 플레이어가 공격 받음");
+        player_hp -= power; //파라미터로 입력받은 power 만큼 hp가 감소함.
+        Debug.Log("[PlayerHealth] : 남은 hp " + player_hp);
+
+        if (player_hp <= 0) //플레이어 목숨이 0이하라면
+        {
+            Die();
+        }
+        else
+        {
+            StartCoroutine(Invincible()); //무적시간 호출
+        }
+        
+    }
+    //플레이어 무적시간
+    private IEnumerator Invincible()
+    {
+        //오브젝트의 레이어를 PlayerDamaged로 변경
+        gameObject.layer = 8;
+        //오브젝트의 색 변경(하얀 투명색)
+        sp.color = new Color(1, 1, 1, 0.4f);
+
+        yield return new WaitForSeconds(2f); //무적시간 2초
+        //오브젝트의 레이어를 Player로 변경
+        gameObject.layer = 6;
+        //오브젝트의 색 원래대로 변경
+        sp.color = new Color(1, 1, 1, 1f);
+    }
+
+    //플레이어 사망
+    public void Die()
+    {
+        Debug.Log("[PlayerHealth] : 플레이어 사망");
+        gameObject.SetActive(false); //플레이어 오브젝트 비활성화
+    }
+    
+}
