ce71122a7ce05d10ef5c4adb89dfd8771ad96ba9
MinJu <jmjchai0509@ewhain.net>
2024-01-23 22:29:32 +0900

    Fix: 캐릭터의 보스 공격 구현 수
    -보스 체력을 플레이어의 콤보 공격 세기에 따라서 깎기
    -보스 공격 성공시 캐릭터 해킹 포인트 +1 오르기
    -boss_hp가 0이 되고 hacking_point를 전부 쌓으면 임무완료(MissionClear) 씬으로 넘어가기

--- EPOH/Assets/Kyoungmin/Scripts/BossManager.cs
+++ EPOH/Assets/Kyoungmin/Scripts/BossManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BossManager : MonoBehaviour
 {
-    public int boss_hp = 1000;
-    public int player_hp = 200;
-    public int hacking_point = 0;
+    public float boss_hp = 1000f;
+    public float player_hp = 200f;
+    public float hacking_point = 0f;
 }
--- EPOH/Assets/Scripts/Player/PlayerBossFight.cs
+++ EPOH/Assets/Scripts/Player/PlayerBossFight.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBossFight : MonoBehaviour
 {
     private PlayerController player_controller;
     private BossManager boss_manager;
+    private AttackArea attack_area;
+    private PlayerAttack player_attack;
+
+    private GameObject player;
+    private GameObject boss;
+    private GameObject attack_area_object;
+
+    private int combo_count = 0;
 
     void Start()
     {
-        player_controller = GetComponent<PlayerController>();
-        boss_manager = FindObjectOfType<BossManager>();
+        player = GameObject.FindWithTag("Player"); 
+        boss = GameObject.FindWithTag("Boss");
 
+        player_controller = player.GetComponent<PlayerController>();
+        boss_manager = boss.GetComponent<BossManager>();
+
+        player_attack = player.GetComponent<PlayerAttack>();  
+
+        
         if (player_controller == null)
         {
             Debug.LogError("PlayerController 스크립트를 찾을 수 없습니다.");
@@ -19,29 +34,80 @@ public class PlayerBossFight : MonoBehaviour
         {
             Debug.LogError("BossManager 스크립트를 찾을 수 없습니다.");
         }
+
+        if (player_attack == null)
+        {
+            Debug.LogError("PlayerAttack 스크립트를 찾을 수 없습니다.");
+        }
+        else{
+            attack_area_object = player.transform.Find("AttackArea").gameObject;
+            attack_area = attack_area_object.GetComponentInChildren<AttackArea>();
+
+            if (attack_area == null)
+            {
+                Debug.LogError("AttackArea 스크립트를 찾을 수 없습니다.");
+            }
+
+        }
+
+        
+        
+        if (player == null)
+        {
+            Debug.LogError("Player GameObject를 찾을 수 없습니다. PlayerBossFight 스크립트에 Player GameObject를 할당해주세요.");
+        }
     }
 
-    void Attack()
+    void attackBoss()
     {
         if (!player_controller.is_attacking)
         {
             player_controller.is_attacking = true;
+            comboCount(); // ComboCount 메서드 호출
 
-            boss_manager.boss_hp -= 20; // 플레이어의 공격을 받으면 boss_hp 값 -20
-            boss_manager.hacking_point += 20; // Boss에게 공격 성공시 hacking_point +20
-
-            //Bossmanager의 boss_hp 값이 0이 되면 더이상 감소하지 않음
-            if (boss_manager.boss_hp <= 0)
+            if (player_attack != null)
             {
-                boss_manager.boss_hp = 0;
-                GameManager.instance.boss_clear_info[GameManager.instance.boss_num] = true;
+
+                // 현재 콤보에 해당하는 공격 세기 설정
+                float current_attack_power = player_attack.combo_attack_power[Mathf.Clamp(combo_count - 1, 0, player_attack.combo_attack_power.Length - 1)];
+
+                // 공격 범위의 공격 세기 설정
+                attack_area.SetAttackPower(current_attack_power);
+
+
+
+                boss_manager.boss_hp -= current_attack_power; // 플레이어의 공격을 받으면 boss_hp 값 공격세기만큼 감소
+                if(boss_manager.boss_hp > 0)
+                {
+                    Debug.Log("boss_hp 값: " + boss_manager.boss_hp);
+                }
+                //Bossmanager의 boss_hp 값이 0이 되면 더이상 감소하지 않음
+                else if(boss_manager.boss_hp <= 0)
+                {
+                    boss_manager.boss_hp = 0;
+                    Debug.Log("boss_hp 값: " + boss_manager.boss_hp);
+                }
+                
+
+                boss_manager.hacking_point += 1; // Boss에게 공격 성공시 hacking_point 값 +1
+                if(boss_manager.hacking_point != 200)
+                {
+                    Debug.Log("hacking_point 값: " + boss_manager.hacking_point);
+                }
+                else if (boss_manager.hacking_point == 200)
+                {
+                    Debug.Log("hacking_point 값: " + boss_manager.hacking_point);   
+                }
+
+                
             }
+
+            player_controller.is_attacking = false;
         }
     }
 
     void Update()
     {
-
         if (Input.GetButtonDown("Teleport"))
         {
             if (player_controller.can_teleport)
@@ -53,37 +119,47 @@ public class PlayerBossFight : MonoBehaviour
             }
         }
 
-        // hacking_point가 200 달성시 임무완료 씬으로 이동
-        if (boss_manager.hacking_point >= 200)
+        // hacking_point= 200 이 되고 boss_hp = 0 이 되면 임무완료 씬으로 이동
+        if (boss_manager != null && boss_manager.hacking_point >= 200 && boss_manager.boss_hp == 0)
         {
-            
+            SceneManager.LoadScene("MissionClear");
+            GameManager.instance.boss_clear_info[GameManager.instance.boss_num] = true;
+        }
+
+        // 공격 버튼 처리
+        if (Input.GetButtonDown("Attack") && !player_controller.is_attacking)
+        {
+            comboCount();
+            attackBoss();
         }
     }
     
 
     // Additional method to handle boss movement and properties
-    void HandleBossMovement()
+    void handleBossMovement()
     {
-        // Assuming you have a reference to the boss object
-        GameObject bossObject = GameObject.Find("Boss"); // Change "Boss" to the actual name of your boss object
-
-        if (bossObject != null)
+        if (boss != null)
         {
-            Rigidbody2D bossRigidbody = bossObject.GetComponent<Rigidbody2D>();
+            Rigidbody2D boss_rigidbody = boss.GetComponent<Rigidbody2D>();
             
-            if (bossRigidbody != null)
+            if (boss_rigidbody != null)
             {
                 // Set the boss object to be a trigger and kinematic
-                bossRigidbody.isKinematic = true;
-                bossRigidbody.gravityScale = 0f;
+                boss_rigidbody.isKinematic = true;
+                boss_rigidbody.gravityScale = 0f;
 
                 // Make the boss move only along the x-axis
-                bossRigidbody.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                boss_rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
             }
         }
         else
         {
             Debug.LogError("Boss 오브젝트를 찾을 수 없습니다.");
         }
+    }
+
+    public void comboCount()
+    {
+        combo_count++;
     }
 }
