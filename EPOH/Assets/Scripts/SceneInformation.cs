5599f66f4abe1849fc7904e69f5ed0fe86b4b18c
Song Ein <songein@ewhain.net>
2024-01-25 17:05:57 +0900

    Feat: SoundManager
    - SoundManager.cs 및 오브젝트 추가
    - 싱글톤 오브젝트
    - 씬이 바뀔 때 배경 음악이 같으면 끊기지 않고 재생되는 기능 구현

--- EPOH/Assets/Scripts/SceneInformation.cs
+++ EPOH/Assets/Scripts/SceneInformation.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class SceneInformation : MonoBehaviour
 {
     public int scene_bgm;
-    private SoundManager sound_manager;
 
     private void Start()
     {
-        sound_manager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
-        sound_manager.PlayBGM(scene_bgm);
+        SoundManager.instance.PlayBGM(scene_bgm);
     }
 
 }
