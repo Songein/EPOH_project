edad958df521aded4ecf7cda50c7f6f43fc51253
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-02-08 11:01:30 +0900

    Feat: Event 1 생성
    -Event 0 끝나고 (튜토리얼용) 메인룸으로 이동하도록 수정
    
    -(튜토리얼용) 주인공이 메인룸에 진입하고나서 주인공 대사창, 튜토리얼창이 순서대로 나오도록 설정

--- EPOH/Assets/Scripts/BeginningEvent.cs
+++ EPOH/Assets/Scripts/BeginningEvent.cs
@@ -8,17 +8,17 @@ using UnityEngine.SceneManagement;
 public class BeginningEvent : MonoBehaviour
 {
     private string text;
-    public TMP_Text targetText;
+    public TMP_Text target_text;
     private float delay = 0.020f;
 
-     private bool eventOccurred = false; // 이벤트 발생 여부를 나타내는 변수
+    private bool event_occurred = false; // 이벤트 발생 여부를 나타내는 변수
 
     void Start()
     {
-        text = targetText.text.ToString();
-        targetText.text = " ";
+        text = target_text.text.ToString();
+        target_text.text = " ";
 
-        if (!eventOccurred) // 이벤트가 발생하지 않았을 때만 실행
+        if (!event_occurred) // 이벤트가 발생하지 않았을 때만 실행
         {
             StartCoroutine(textPrint(delay));
         }
@@ -32,7 +32,7 @@ public class BeginningEvent : MonoBehaviour
         {
             if (count < text.Length)
             {
-                targetText.text += text[count].ToString();
+                target_text.text += text[count].ToString();
                 count++;
             }
 
@@ -40,9 +40,9 @@ public class BeginningEvent : MonoBehaviour
         }
 
         // 이벤트 출력이 끝나면 MainRoom으로 전환
-        SceneManager.LoadScene("MainRoom");
+        SceneManager.LoadScene("Tutorial MainRoom");
         
-        eventOccurred = true; // 이벤트가 발생했음을 표시
+        event_occurred = true; // 이벤트가 발생했음을 표시
         gameObject.SetActive(false); // 스크립트를 비활성화하여 더 이상 이벤트가 발생하지 않도록 함
     }
 }
