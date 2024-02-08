c42b230ca5ac2aacf0627326cc10289d26d9355c
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-02-09 02:06:03 +0900

    Feat: Event 2 생성
    -주인공이 특정 범위 들어올 경우 Event 2가 시작하도록 설정
    -Event 1 스크립트(Event1Script.cs) 주석 수정

--- EPOH/Assets/Scripts/TutorialEvent/Event1Script.cs
+++ EPOH/Assets/Scripts/TutorialEvent/Event1Script.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Event1Script : MonoBehaviour
 {
-    public GameObject character_panel; // 주인공 Panel을 저장할 변수
-    public TMP_Text dialogue_text; // 주인공 TMPro Text 요소를 저장할 변수
+    public GameObject character_panel; // 주인공 대화창 Panel을 저장할 변수
+    public TMP_Text dialogue_text; // 주인공 Dialogue TMPro Text 요소를 저장할 변수
     private float delay_between_lines = 2f; // 대사 사이의 딜레이
 
     public GameObject tutorial_panel; //  튜토리얼 Panel을 저장할 변수
@@ -22,14 +22,14 @@ public class Event1Script : MonoBehaviour
     IEnumerator startCharacterEvent()
     {
 
-        character_panel.SetActive(true); // 주인공 Panel 활성화
+        character_panel.SetActive(true); // 주인공 대화창 Panel 활성화
         tutorial_panel.SetActive(false); // 튜토리얼 Panel 비활성화
 
         //메인룸에서 눈을 뜨는 주인공
         Debug.Log("눈을 뜨는 주인공 애니메이션");
 
         // 첫 번째 대사 표시
-        dialogue_text.text = "H???: 여긴…?";
+        dialogue_text.text = "주인공: 여긴…?";
         yield return new WaitForSeconds(delay_between_lines);
 
         // 대사 사라짐
@@ -40,13 +40,13 @@ public class Event1Script : MonoBehaviour
 
         // 두 번째 대사 표시
         yield return new WaitForSeconds(0.5f); // 대사 간의 짧은 딜레이
-        dialogue_text.text = "H???: 아무 것도 기억나지 않아…";
+        dialogue_text.text = "주인공: 아무 것도 기억나지 않아…";
         yield return new WaitForSeconds(delay_between_lines);
 
         // 대사 사라짐
         dialogue_text.text = "";
 
-        // 주인공 Panel 비활성화
+        // 주인공 대화창 Panel 비활성화
         character_panel.SetActive(false);
 
 
@@ -56,9 +56,9 @@ public class Event1Script : MonoBehaviour
 
         yield return null; // 한 프레임 대기
 
-        if (tutorial_panel.activeSelf) // 이벤트가 발생하지 않았을 때만 실행
+        if (tutorial_panel.activeSelf) 
         {
-            StartCoroutine(tutorialTextPrint());
+            StartCoroutine(tutorialTextPrint()); // 튜토리얼 이벤트 발생
         }
 
     }
@@ -68,6 +68,7 @@ public class Event1Script : MonoBehaviour
         tutorial_text.text = "튜토리얼 - 좌우 방향키로 이동할 수 있습니다. Z키로 점프할 수 있습니다. 점프는 최대 2번까지 연속으로 가능합니다.";
         yield return new WaitForSeconds(tutorial_delay);
         tutorial_panel.SetActive(false); // 튜토리얼 Panel 비활성화
+        Debug.Log("튜토리얼 panel 비활성화");
     }
 
 }
