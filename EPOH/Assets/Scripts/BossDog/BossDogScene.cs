ed5c46777cf623690f26b84e4f04df85e0aed69d
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2024-02-09 22:29:39 +0900

    Feat : Event5 완료

--- EPOH/Assets/Scripts/BossDog/BossDogScene.cs
+++ EPOH/Assets/Scripts/BossDog/BossDogScene.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossDogScene : MonoBehaviour
 {
     [SerializeField] private GameObject main_camera;
     [SerializeField] private GameObject sub_camera;
-
-    private bool camera_move_event1;
-    private bool event2_end = false;
+    [SerializeField] private GameObject tutorial_text;
+    
+    [SerializeField] bool camera_move_event1;
+    [SerializeField] bool event2_end = false;
+    [SerializeField] bool tutorial_end = false;
+    private Vector3 pos;
     private Vector3 destination = new Vector3(5.8f, -3f, -10f);
-
     private TalkAction talk_action;
 
     [SerializeField] private bool battle_start;
@@ -22,6 +26,8 @@ public class BossDogScene : MonoBehaviour
         //메인 카메라(주인공) 비활성화, 서브 카메라(보스) 활성화
         main_camera.SetActive(false);
         sub_camera.SetActive(true);
+        tutorial_text.SetActive(false);
+        pos = tutorial_text.transform.position;
         //카메라 움직임 이벤트1 실행
         camera_move_event1 = true;
         //튜토리얼 끝나기 전까지 battle_start 막기
@@ -56,9 +62,21 @@ public class BossDogScene : MonoBehaviour
             talk_action.Action();
             event2_end = true;
         }
-        
-        
+
+        if (GameManager.instance.story_info == 8 && !tutorial_end)
+        {
+            tutorial_text.SetActive(true);
+            Vector3 dir_pos = pos;
+            //0.3f 거리 내에서 -1f ~ 1f 만큼 위아래로 이동 효과
+            dir_pos.y = pos.y + 0.3f * Mathf.Sin(Time.time * 1f);
+            tutorial_text.transform.position = dir_pos;
+        }
         
     }
-    
+
+    public void EndTutorial()
+    {
+        tutorial_text.SetActive(false);
+        tutorial_end = true;
+    }
 }
