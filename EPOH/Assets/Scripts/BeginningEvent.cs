b1baa841b2a62b2831bbe0f82c3d132b3251ee59
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-02-10 16:21:29 +0900

    Fix: Event 0 마지막 두 줄 속도 수정
    ......" 과 "프로세스 완료"가 출력될 때 1초 딜레이 시간 추가

--- EPOH/Assets/Scripts/BeginningEvent.cs
+++ EPOH/Assets/Scripts/BeginningEvent.cs
@@ -4,48 +4,49 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class BeginningEvent : MonoBehaviour
 {
-    private string text;
-    public TMP_Text target_text;
-    private float delay = 0.020f;
+    public TMP_Text b_text;
 
-    private bool event_occurred = false; // 이벤트 발생 여부를 나타내는 변수
+    private string[] messages = 
+    {
+        "대상 식별중…",
+        "E801F83499F1D00AE6B6451C445B013880A9906EF783C86EFB671E1CE74C3842CCE97B597E16546AABDE44AB50E816027E1E08F44E297BF89E41C61B1807F6C2",
+        "상태: Trance",
+        "의식 연결을 시작합니다.",
+        "공유 에어리어 접속",
+        "아바타 구현화",
+        "컴포넌트 H 부착",
+        "......",
+        "프로세스 완료"
+    };
 
     void Start()
     {
-        text = target_text.text.ToString();
-        target_text.text = " ";
-
-        if (!event_occurred) // 이벤트가 발생하지 않았을 때만 실행
-        {
-            StartCoroutine(textPrint(delay));
-        }
+        StartCoroutine(displayMessages());
     }
 
-      IEnumerator textPrint(float d)
+    IEnumerator displayMessages()
     {
-        int count = 0;
-
-        while (count != text.Length)
+        for (int j = 0; j < messages.Length; j++)
         {
-            if (count < text.Length)
+            string message = messages[j];
+            for (int i = 0; i < message.Length; i++)
             {
-                target_text.text += text[count].ToString();
-                count++;
+                b_text.text += message[i]; // Add one character at a time
+                yield return new WaitForSeconds(0.04f); // adjust speed here
+            }
+            if (j < messages.Length - 1) // Don't add newline at the end
+            {
+                b_text.text += "\n"; // Start a new line for each message
+            }
+            if (message == "......" || message == "프로세스 완료")
+            {
+                yield return new WaitForSeconds(1f); 
             }
 
-            yield return new WaitForSeconds(delay);
         }
 
-        // 이벤트 출력이 끝나면 MainRoom으로 전환
         SceneManager.LoadScene("Tutorial MainRoom");
-        
-        event_occurred = true; // 이벤트가 발생했음을 표시
-        gameObject.SetActive(false); // 스크립트를 비활성화하여 더 이상 이벤트가 발생하지 않도록 함
     }
 }
-
-    
-
