7a2d3d945b9a062db2ea1b0c5ff4f87618dabb5c
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-02-13 13:31:00 +0900

    Feat: 플레이어 공격(콤보1,2,3) 효과음 부착

--- EPOH/Assets/Scripts/Player/Attack/AttackBehaviour.cs
+++ EPOH/Assets/Scripts/Player/Attack/AttackBehaviour.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 
 public class AttackBehaviour : StateMachineBehaviour
 {
+    public AudioClip attack1SelectClip; // 공격1 선택음
+    public AudioClip attack2SelectClip; // 공격2 선택음
+    public AudioClip attack3SelectClip; // 공격3 선택음
+
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //AudioSource 공격음 
+        AudioSource audioSource = animator.GetComponent<AudioSource>();
+
         //Attack Area 오브젝트 찾기(비활성화 오브젝트이기에 부모 오브젝트인 Player 오브젝트를 통해 접근
         GameObject attack_area = GameObject.Find("Player").transform.Find("AttackArea").gameObject;
         PlayerController pr = FindObjectOfType<PlayerController>(); //PlayerController 할당
@@ -18,14 +26,45 @@ public class AttackBehaviour : StateMachineBehaviour
         if (stateInfo.IsName("Attack One"))
         {
             attack_area.GetComponent<AttackArea>().SetAttackPower(PlayerAttack.instance.combo_attack_power[0]);
+
+            if (attack1SelectClip != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(attack1SelectClip);
+            }
+            else
+            {
+                Debug.LogWarning("attack1SelectClip이나 AudioSource가 null입니다.");
+            }
+            
+            
         }
         else if (stateInfo.IsName("Attack Two"))
         {
             attack_area.GetComponent<AttackArea>().SetAttackPower(PlayerAttack.instance.combo_attack_power[1]);
+            
+            if (attack2SelectClip != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(attack2SelectClip);
+            }
+            else
+            {
+                Debug.LogWarning("attack2SelectClip이나 AudioSource가 null입니다.");
+            }
+            
         }
         else if (stateInfo.IsName("Attack Three"))
         {
             attack_area.GetComponent<AttackArea>().SetAttackPower(PlayerAttack.instance.combo_attack_power[2]);
+            
+            if (attack3SelectClip != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(attack3SelectClip);
+            }
+            else
+            {
+                Debug.LogWarning("attack3SelectClip이나 AudioSource가 null입니다.");
+            }
+            
         }
     }
 
