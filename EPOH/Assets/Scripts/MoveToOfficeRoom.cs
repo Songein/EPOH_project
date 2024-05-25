3d73aac63d8da7dc80160ef385a9e9801c553ffa
judymoody59 <jmj.chai@gmail.com>
2024-05-26 03:58:06 +0900

    Feat: 메인룸에 OfficePortal 추가
    메인룸에서 회사 공간 1~3 으로 이동할 수 있는 Office Portal 추가

---
+++ EPOH/Assets/Scripts/MoveToOfficeRoom.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToOfficeRoom : MonoBehaviour
+{
+    public void officeSceneChange()
+    {
+        string[] OR = {"OfficeRoom1", "OfficeRoom2", "OfficeRoom3"}; 
+
+        if (GameManager.instance != null && GameManager.instance.office_room >= 0 && GameManager.instance.office_room < OR.Length)
+        {
+            string office_scene_name = OR[GameManager.instance.office_room];
+
+            SceneManager.LoadScene(office_scene_name);
+        }
+        else
+        {
+            Debug.LogError("Invalid office_room index or GameManager instance is null.");
+        }
+        
+    }
+}
+
+
+
+
