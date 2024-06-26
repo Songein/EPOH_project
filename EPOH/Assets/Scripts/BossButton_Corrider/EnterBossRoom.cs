e11cfd5a5cd0918897949b1297b0d8e21dc4d674
judymoody59 <jmj.chai@gmail.com>
2024-06-26 14:31:48 +0900

    Feat: OfficeRoom 구현2

--- EPOH/Assets/Scripts/BossButton_Corrider/EnterBossRoom.cs
+++ EPOH/Assets/Scripts/BossButton_Corrider/EnterBossRoom.cs
@@ -4,42 +4,38 @@ using UnityEngine.SceneManagement;
 
 public class EnterBossRoom : MonoBehaviour
 {
-    //private GameManager game_manager; // GameManager 스크립트에 대한 참조 변수
-    /*public Button move_to_right_button; // MoveToRight device 버튼
-    */
-
-    void Start(){
-        /*
-        if (move_to_right_button != null){
-            move_to_right_button.onClick.AddListener(() => moveToBossRoom());
-        }*/
-    }
-
+    PlayerController playerController;
 
     public void moveToBossRoom()
     {
-        
-        if (GameManager.instance != null)
-        { 
-            int boss_index = GameManager.instance.boss_num;
-            string boss_room_scene_name = "";
+        playerController = GetComponent<PlayerController>();
 
-            if (boss_index >= 0 && boss_index < GameManager.boss_cnt) 
+
+        // 현재 활성화된 씬 가져오기
+        Scene current_scene = SceneManager.GetActiveScene();
+        
+        if (GameManager.instance != null && current_scene.name == "OfficeRoom1")
+        {
+            if (GameManager.instance.boss_clear_info[0] && this.gameObject.name == "BossDog")
             {
-                boss_room_scene_name = "BossRoom" + GameManager.instance.boss_info[boss_index, 0]; // 보스룸 씬 이름
-                Debug.Log("Boss Room Scene Name: " + boss_room_scene_name);
+                Debug.Log("BossDog와 상호작용했습니다.");
+                // 보스룸으로 이동
+                SceneManager.LoadScene("BossRoomDog");
+                Debug.Log("BossRoomDog Scene으로 이동");
+            }
+            else if (GameManager.instance.boss_clear_info[1] && this.gameObject.name == "BossPartTime")
+            {
+                // 보스룸으로 이동
+                SceneManager.LoadScene("BossRoomPartTime");
+                Debug.Log("BossRoomPartTime Scene으로 이동");
             }
             else
             {
-                Debug.LogWarning("해당하는 보스룸 씬을 찾을 수 없습니다.");
-                return;
+                Debug.LogWarning("조건에 맞지 않거나 GameManager instance를 찾을 수 없습니다.");
             }
-            Debug.Log(boss_room_scene_name);
-
-            // 보스룸으로 이동
-            SceneManager.LoadScene(boss_room_scene_name); 
         }
         else{
+            playerController.is_interacting = false;
             Debug.LogWarning("GameManager instance not found.");
         }
         
--- EPOH/Assets/Scripts/Interact/BossRoomInteract.cs
+++ EPOH/Assets/Scripts/Interact/BossRoomInteract.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BossRoomInteract : Interaction
+public class CorriderRightInteract : Interaction
 {
     EnterBossRoom enterBossRoom;
     
