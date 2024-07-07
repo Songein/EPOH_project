71ea65a05ac85e808ab67e324b6a6e3e005b5b5d
judymoody59 <jmj.chai@gmail.com>
2024-07-07 13:16:52 +0900

    Feat: BossDog Scene1

--- EPOH/Assets/Scripts/Hacking.cs
+++ EPOH/Assets/Scripts/Hacking.cs
@@ -49,15 +49,15 @@ public class Hacking : MonoBehaviour
     }
 
     // 보스전 종료 함수
-    /*
     public void endBossBattle()
     {
         // hacking_point= 200 이 되고 boss_hp = 0 이 되면 임무완료 씬으로 이동
-        SceneManager.LoadScene("MissionClear");
+        SceneManager.LoadScene("OfficeRoom" + GameManager.instance.office_room);
 
         Debug.Log("보스전 종료");
     }
 
+    /*
     // 보스 클리어 정보 갱신
     private void updateBossClearInfo()
     {
--- EPOH/Assets/Scripts/SceneManager/BossDogScene.cs
+++ EPOH/Assets/Scripts/SceneManager/BossDogScene.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossDogScene : MonoBehaviour
 {
+    public BossManager boss_manager;
+    public Hacking hacking;
+    public bool battle_start; // 배틀 시작
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        battle_start = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+    
+        if (battle_start && boss_manager.hacking_point == 200)
+        {
+            battle_start = false;
+            GameManager.instance.office_room = 1;
+            hacking.endBossBattle();
+        }
+
+        if (battle_start && boss_manager.player_hp == 0)
+        {
+            playerDeath();
+        }
+    }
+
+    public void playerDeath()
+    {
+        battle_start = false;
+        SceneManager.LoadScene("OfficeRoom1");
         
     }
 }
