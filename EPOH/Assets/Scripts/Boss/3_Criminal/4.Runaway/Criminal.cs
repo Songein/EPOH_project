60b27c38c42b16fd8db9b6e9486bc5bffd0bd3a9
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2025-04-27 12:54:21 +0900

    Feat: Runaway 구현
    sprite 넣은 애니메이션 추가

--- EPOH/Assets/Scripts/Boss/3_Criminal/4.Runaway/Criminal.cs
+++ EPOH/Assets/Scripts/Boss/3_Criminal/4.Runaway/Criminal.cs
@@ -10,9 +10,11 @@ public class Criminal : Interactable
     [SerializeField] private float _increaseHackPoint;
     [SerializeField] private float _decreaseHackPoint;
     [SerializeField] private float _explosionDamage;
+    private Animator animator;
     private void Awake()
     {
         canInteract = false;
+         animator = GetComponent<Animator>();
     }
 
     public override void OnInteract()
@@ -24,6 +26,7 @@ public class Criminal : Interactable
         if (_isRealCriminal)
         {
             // 1. 범죄자 깜짝 놀라는 애니메이션 실행
+            animator.SetTrigger("Notice");
             // 2. 해킹 포인트 증가
             BossManagerNew.Current.OnIncreaseHackingPoint?.Invoke(_increaseHackPoint);
             
@@ -34,6 +37,10 @@ public class Criminal : Interactable
         {
             // 가짜 범죄자이면 폭발 & 데미지 입고 해킹포인트 감소
             // 1. 폭발 애니메이션 실행
+            if (!_isRealCriminal) {
+                animator.SetTrigger("Explosion");
+            }
+
             // 2. 데미지 입고 해킹포인트 감소
             _player.GetComponent<PlayerHealth>().Damage(_explosionDamage);
             BossManagerNew.Current.OnDecreaseHackingPoint?.Invoke(_decreaseHackPoint);
@@ -41,17 +48,61 @@ public class Criminal : Interactable
             // 진짜 범죄자 먼저 사라짐
             DestroyOnlyReal();
             // 그 후 나머지 범죄자 사라짐
-            DestroyAllCriminal();
+           Invoke("DestroyAllCriminal",2f);
         }
     }
 
+    IEnumerator DestroyCriminal() {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("DestroyCriminal");
+        Destroy(gameObject);
+        //gameObject.SetActive(false);
+    }
 
+
+    IEnumerator DestroyRealCriminal()
+    {
+        if (_isRealCriminal)
+        {
+            animator.SetTrigger("Laugh");
+            yield return new WaitForSeconds(1f); //2초 후 사라짐 1초 + destroyCriminal 1초;
+            Debug.Log("OnlyRealDie");
+            StartCoroutine(DestroyCriminal());
+        }
+    }
+
+    private void DestroyAllCriminal()
+    {
+        Criminal[] foundObjects = FindObjectsOfType<Criminal>();
+
+        foreach (var criminal in foundObjects)
+        {
+            if (criminal == null) return;
+            criminal.StartCoroutine(criminal.DestroyCriminal());
+        }
+    }
+
+    private void DestroyOnlyReal()
+    {
+        Criminal[] foundObjects = FindObjectsOfType<Criminal>();
+
+        foreach (var criminal in foundObjects)
+        {
+            if (criminal == null) return;
+
+            criminal.StartCoroutine(criminal.DestroyRealCriminal());
+        }
+    }
+
+    /*
     private void DestroyCriminal()
     {
         // 사라지는 애니메이션 실행
         // 씬 내에서 파괴
+        Debug.Log("DestroyCriminal");
         gameObject.SetActive(false);
     }
+    
 
     public void DestroyRealCriminal()
     {
@@ -82,4 +133,5 @@ public class Criminal : Interactable
             criminal.DestroyRealCriminal();
         }
     }
+    */
 }
